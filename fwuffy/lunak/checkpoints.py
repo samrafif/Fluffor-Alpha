26e194b78224cded01dbc17ee3a7e94bcdc2f0be
Mallow <72262190+mallowv@users.noreply.github.com>
2023-04-07 22:56:05 +0700

    🐛 Bugfix, Checkpoint file already exists

--- fwuffy/lunak/checkpoints.py
+++ fwuffy/lunak/checkpoints.py
@@ -1,12 +1,14 @@
 import pathlib
 
+
 class Checkpointer:
     def __init__(self, path, step=0):
         self.path = pathlib.Path(path)
         self.step = step
-        
-        self.path.mkdir()
-        
+
+        if not self.path.exists:
+            self.path.mkdir()
+
     def save(self, net):
         net.save(self.path / f"ckpt-{self.step}")
         self.step += 1
